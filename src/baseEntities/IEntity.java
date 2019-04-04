614a0fa0230578bc441bae381a9a9437d189f7fc
AJQM124 <47283174+AJQM124@users.noreply.github.com>
2019-04-04 12:45:49 -0400

    Updated

--- src/baseEntities/IEntity.java
+++ src/baseEntities/IEntity.java
@@ -5,8 +5,8 @@ package baseEntities;
  /**
   * @author kkontog, ktsiouni, mgrigori
   *  
-  * Base Interface for Publisher and Subscriber classes
+  * Base Interface for Publisher and Subscriber classes. They both contain the same basic things. 
   */
 public interface IEntity {
-
+ 
 }
